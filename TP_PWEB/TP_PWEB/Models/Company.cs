fc5af1b5d730dcb07dbd44498e7d65cb8b547dbc
Rui Canas <ruijfcanas@gmail.com>
2020-12-24 04:53:35 +0000

    Final fix

---
+++ TP_PWEB/TP_PWEB/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TP_PWEB.Models
+{
+    public class Company
+    {
+        [Key]
+        public int IDCompany { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TP_PWEB/TP_PWEB/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TP_PWEB.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int IDVehicle { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Category { get; set; }
+        public int NumberKm { get; set; }
+        public float VehicleTank { get; set; }
+        public bool Damages { get; set; }
+        public double Price { get; set; }
+
+        public virtual Company Company { get; set; }
+    }
+}
