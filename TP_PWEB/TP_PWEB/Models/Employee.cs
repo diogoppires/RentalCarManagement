97b2c3beddbf8113bd2ee5cf78c749e892c387a9
Rui Canas <ruijfcanas@gmail.com>
2020-12-24 04:28:45 +0000

    Manage Employees and BussinesAdmins implemented

---
+++ TP_PWEB/TP_PWEB/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TP_PWEB.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int idEmployee { get; set; }
+
+        public virtual Company idCompany{ get; set; }
+        public virtual ApplicationUser idUser { get; set; }
+
+    }
+}
