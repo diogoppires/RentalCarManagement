04c1d3354353f959f6314cdb18af2fd998c57a72
Rui Canas <ruijfcanas@gmail.com>
2021-01-06 23:56:01 +0000

    Last Changes

---
+++ TP_PWEB/TP_PWEB/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TP_PWEB.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int idBooking { get; set; }
+        public DateTime bookingInit { get; set; }
+        public DateTime bookingEnd { get; set; }
+        public virtual Vehicle vehicle { get; set; }
+    }
+}
