0f4da8c35f231d9624340d6b0046efad1b6e1616
Diogo <dpinho07@hotmail.com>
2021-01-13 03:04:22 +0000

    Booking on Employees logic

---
+++ TP_PWEB/TP_PWEB/Models/BookingState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TP_PWEB.Models
+{
+    public class BookingState
+    {
+        public Booking Booking { get; set; }
+        public bool approved { get; set; }
+    }
+}
